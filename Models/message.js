7b2296543b535599009a6a6df854123e7ddebb04
ThanhNam20 <khuatthanhnam@gmail.com>
2019-08-22 16:38:17 +0700

    Repairing some things

--- Models/message.js
+++ Models/message.js
@@ -1,37 +1,53 @@
 let messages = [
   {
     uid: "khuatthanhnam@gmail.com",
-    message:"Tình là trò chơi ân ái sao nghe trái ngang",
+    message: "Tình là trò chơi ân ái sao nghe trái ngang",
     time: "5pm Sat"
   },
   {
     uid: "khuatson71@gmail.com",
-    message:"We've all experienced that moment of accidentally sending a text message we wish we could take back. Many of us have probably even received text messages that weren't ever meant for us.",
+    message:
+      "We've all experienced that moment of accidentally sending a text message we wish we could take back. Many of us have probably even received text messages that weren't ever meant for us.",
     time: "10pm Sat"
   }
 ];
 
-const listSubscriber =[];
+const listSubscriber = [];
 
-function subscribe(screen){
-  listSubscriber.push(screen); 
+function subscribe(screen) {
+  listSubscriber.push(screen);
 }
 
-function unSubscriber() {
+function unSubscribe() {}
 
-}
-
-function notifyMessage(message,time) {
-  for(let i=0; i<listSubscriber.length; i++) {
-    listSubscriber[i].onNotifyMessage(message,time);
+function notifyMessage(message, time) {
+  for (let i = 0; i < listSubscriber.length; i++) {
+    listSubscriber[i].onNotifyMessage(message, time);
   }
 }
 
-
-function addMessage(message,time) {
-  messages.push(message,time);
-  notifyMessage(message,time)
+function addMessage(message, time) {
+  messages.push(message, time);
+  notifyMessage(message, time);
+  saveMessage(message,time)
 }
 
-export {addMessage,subscribe};
+function saveMessage() {
+  // Add a new document in collection "messages"
+  db.collection("messages")
+    .doc()
+    .update({
+     uid : massages.uid,
+     message: messages.message,
+     time: messages.time
+    })
+    .then(function() {
+      console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+      console.error("Error writing document: ", error);
+    });
+}
+
+export { addMessage, subscribe };
 export default messages;
